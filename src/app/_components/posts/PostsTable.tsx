85ae9dd050f1263d156f68efaaa33adfc173627f
Penko Atanasov <atanasovpenko@gmail.com>
2025-01-23 20:07:01 +0200

    Started updating the post table

--- src/app/_components/posts/PostsTable.tsx
+++ src/app/_components/posts/PostsTable.tsx
@@ -1,3 +1,34 @@
-export function PostsTable() {
-  return <>Posts Table Here</>;
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/ui/table";
+
+type PostTableProps = {
+  limit?: number;
+  title?: string;
+};
+
+export function PostsTable({ limit, title }: PostTableProps) {
+  return (
+    <div className="mt-10">
+      <h3 className="mb-4 text-2xl font-semibold">{title ?? "Posts"}</h3>
+      <Table>
+        <TableCaption>A list of recent posts</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Title</TableHead>
+            <TableHead className="hidden md:table-cell">Author</TableHead>
+            <TableHead className="hidden text-right md:table-cell">
+              Date
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>{/* map through the posts */}</TableBody>
+      </Table>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ import DashboardCard from "~/app/_components/dashboard/DashboardCard";
 import { Button } from "~/components/ui/button";
 import { getServerAuthSession } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
+import { PostsTable } from "./_components/posts/PostsTable";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
@@ -58,6 +59,7 @@ export default async function Home() {
           }
         />
       </div>
+      <PostsTable title="Latest Posts" />
       {/* <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
