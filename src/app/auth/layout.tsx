9ea68aff1ff8ff88a8eceee4f45e4a72a68145dc
Penko Atanasov <atanasovpenko@gmail.com>
2025-02-02 17:55:30 +0200

    Adding toggle on auth page

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,8 +1,13 @@
+import ThemeToggler from "~/components/ThemeToggler";
+
 export default function AuthLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className="relative flex h-[100vh] items-center justify-center">
+      <div className="absolute bottom-5 right-0 text-white">
+        <ThemeToggler />
+      </div>
       {children}
     </div>
   );
