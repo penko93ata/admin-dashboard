1cf0f81b5f3c6b18321e7a643e7cd018186167c3
Penko Atanasov <atanasovpenko@gmail.com>
2025-02-02 21:44:34 +0200

    Adding new table pagination + actions column

--- src/app/(main)/posts/columns.tsx
+++ src/app/(main)/posts/columns.tsx
@@ -1,6 +1,15 @@
 "use client";
 
 import { type ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+import { Button } from "~/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
 import { type SelectPost } from "~/types/posts";
 
 export interface ColumnMeta {
@@ -19,5 +28,30 @@ export const columns: ColumnDef<SelectPost>[] = [
     header: () => <div className="hidden text-right md:table-cell">Date</div>,
     cell: ({ row }) => new Date(row.getValue("createdAt")).toLocaleDateString(),
     meta: { columnClasses: "hidden text-right md:table-cell" } as ColumnMeta,
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const post = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(post.id.toString())}
+            >
+              Copy Post ID
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
